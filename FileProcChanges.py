8aeefed4d70040909781d01eaf8a6f25c89497b9
Ken B <160813236+KenB773@users.noreply.github.com>
2025-03-26 16:41:54 -0400

    Update FileProcChanges.py

--- FileProcChanges.py
+++ FileProcChanges.py
@@ -2,11 +2,9 @@ import os, time
 
 start = time.time()
 
-# ...existing code...
-
 file_type = os.path.splitext(key)[1].lower()
 duration = round(time.time() - start, 3)
 
-# Include these in your DynamoDB put_item:
+# DynamoDB put_item:
 'FileType': file_type,
 'Duration': duration
